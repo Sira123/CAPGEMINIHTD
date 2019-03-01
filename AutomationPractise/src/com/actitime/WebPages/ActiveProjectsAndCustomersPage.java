4ff415d3f69575f26f2d37c029101c67c12a6e57
harrshaa <harrshaa@DESKTOP-HHTFBOV>
2019-03-01 09:47:50 +0530

    1st commit

---
+++ AutomationPractise/src/com/actitime/WebPages/ActiveProjectsAndCustomersPage.java
@@ -0,0 +1,38 @@
+package com.actitime.WebPages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.actitime.UtilLibrary.SuperTestScript;
+
+public class ActiveProjectsAndCustomersPage {
+	
+	
+	@FindBy(xpath="//input[@value='Add New Customer']")
+	private WebElement addNewCustomerButton;
+
+	@FindBy(className="successmsg")
+	private WebElement successMessage;
+	
+	public ActiveProjectsAndCustomersPage()
+	{
+		PageFactory.initElements(SuperTestScript.driver, this);
+	}
+	
+	public void clickOnAddNewCustomer()
+	{
+		addNewCustomerButton.click();
+	}
+	
+	public String getSuccessMessage()
+	{
+		return successMessage.getText();
+	}
+	
+}
+
+
+
+
+
---
+++ AutomationPractise/src/com/actitime/WebPages/OpenTaskPage.java
@@ -0,0 +1,43 @@
+package com.actitime.WebPages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.actitime.UtilLibrary.SuperTestScript;
+
+public class OpenTaskPage {
+	
+	
+	@FindBy(className="logoutImg")
+	private WebElement logoutButton;
+	
+	@FindBy(linkText="Projects & Customers")
+	private WebElement projectAndCustomersLink;
+	
+	
+	public OpenTaskPage()
+	{
+		PageFactory.initElements(SuperTestScript.driver, this);
+	}
+	public String retrieveTitleOfWebPage()
+	{
+	
+		String data = SuperTestScript.driver.getTitle();
+		return data;
+		
+	}
+	public void clickOnProjectsAndCustomersLink()
+	{
+		projectAndCustomersLink.click();
+	}
+	
+	public void clickOnLogoutButton(){
+		
+		logoutButton.click();
+		
+		
+	}
+	
+
+}
